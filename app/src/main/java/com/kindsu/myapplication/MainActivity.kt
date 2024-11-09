71134e8c926695c04d5080cfd63192ad25500f53
Kindsu <KindsuCSM@users.noreply.github.com>
2024-11-09 21:27:21 +0100

    update

--- app/src/main/java/com/kindsu/myapplication/MainActivity.kt
+++ app/src/main/java/com/kindsu/myapplication/MainActivity.kt
@@ -1,20 +1,94 @@
 package com.kindsu.myapplication
 
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kindsu.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var lstPastillas: MutableMap<String, List<String>>
+    private lateinit var lstHoras: MutableMap<String, String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        lstPastillas = mutableMapOf(
+            "14/11/2024" to mutableListOf("Tirodrill", "Furosemida", "Ferbisol"),
+            "10/11/2024" to mutableListOf("Omeprazol", "Trajenta", "Apixaban"),
+            "11/11/2024" to mutableListOf("Bisoprolol", "Loxartan", "Alodipino")
+        )
+
+        lstHoras = mutableMapOf(
+            "14/11/2024" to "13:50",
+            "10/11/2024" to "14:50",
+            "11/11/2024" to "16:50",
+        )
+        showCurrentDate() // Mostrar la fecha actual y las pastillas al inicio
+    }
+
+    //Recibo un parametro tipo calendario y lo convierto a string recogiendo sus valores dia, mes y año
+    private fun formatDate(calendar: Calendar): String {
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH) + 1
+        val year = calendar.get(Calendar.YEAR)
+        return "$day/$month/$year"
+    }
+
+
+    private fun showCurrentDate() {                                                 // Mostrar la fecha actual en el TextView al iniciar la actividad
+        val calendar = Calendar.getInstance()
+        val currentDate = formatDate(calendar)
+
+        addCheckBox(currentDate)                                                    // Mostrar la lista de pastillas para la fecha actual
+        binding.tvFecha.text = currentDate                                          // Mostrar la fecha actual
+        binding.cvCalendar.setOnDateChangeListener{ _, year, month, dayOfMonth ->  // Configurar el listener para cuando el usuario seleccione una fecha
+            val selectedDate = Calendar.getInstance().apply {                       // Recogemos la fecha que indica el usuario
+                set(year, month, dayOfMonth) }
+            val formattedDate = formatDate(selectedDate)                            //Formatear la fecha que hemos recogido de Calendar a string
+            binding.tvFecha.text = formattedDate                                    // Actualizar la fecha en el TextView
+            addCheckBox(formattedDate)                                              // Mostrar las pastillas asociadas a la fecha seleccionada
+        }
+    }
+
+    private fun addCheckBox(date: String) {
+        val txtColor = ContextCompat.getColor(this, R.color.txtColor)       //Recogemos el color del xml colors y se lo damos a una variable para darselo al checkbox
+        val pastis = lstPastillas[date] ?: listOf("No tienes que tomar pastillas")  //la clave fecha no existe en el diccionario, mostraremos un checkbox con el texto
+
+
+        binding.layoutCheckBox.removeAllViews()                                     // Limpiar cualquier CheckBox previo en el contenedor
+
+        // Añadir los CheckBox al layout
+        for (pasti in pastis) {
+            val checkbox = CheckBox(this)
+            checkbox.text = pasti
+            checkbox.textSize = 16f
+            checkbox.setTextColor(txtColor)
+            checkbox.setTypeface(null, Typeface.BOLD)
+            checkbox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                checkbox. paintFlags =  Paint.STRIKE_THRU_TEXT_FLAG
+            } else {
+                checkbox. paintFlags = checkbox.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+            }
+        }
+            binding.layoutCheckBox.addView(checkbox)
         }
     }
 }
